4fe3a8798314da66ab66ff5cfd0e0e6f7983a329
Andrew <wilson.andrew008@gmail.com>
2018-11-26 20:06:26 -0600

    Add navbar

--- buy-r-games/src/App.js
+++ buy-r-games/src/App.js
@@ -1,23 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './App.css';
-import './Include/bootstrap';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { CehckoutComponent } from './Checkout.Component/CheckoutComponent';
-import { HomePage } from './HomePage.Component/HomePage';
+import "./App.css";
+import "./Include/bootstrap";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { CehckoutComponent } from "./Checkout.Component/CheckoutComponent";
+import { HomePage } from "./HomePage.Component/HomePage";
+import { AppNav } from "./Nav.Component/Nav.component.js";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div id="main-content-container">
-          <Switch>
-          <Route path = "/checkout" component={CehckoutComponent} />
-          {/* default */}
-          <Route component={HomePage} />
-          </Switch>
-        </div>
-    </BrowserRouter>
+        <>
+          <AppNav />
+          <div id="main-content-container">
+            <Switch>
+              <Route path="/checkout" component={CehckoutComponent} />
+              {/* default */}
+              <Route component={HomePage} />
+            </Switch>
+          </div>
+        </>
+      </BrowserRouter>
     );
   }
 }
