5e528c59e769b01092abff1948d5776519a2f700
shamikulamin <shamikulamin@gmail.com>
2018-11-27 12:26:29 -0600

    conflict fix

--- buy-r-games/src/App.js
+++ buy-r-games/src/App.js
@@ -1,41 +1,22 @@
 
-<<<<<<< HEAD
 import './App.css';
 import './Include/bootstrap';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { CehckoutComponent } from './Checkout.Component/CheckoutComponent';
 import { HomePage } from './HomePage.Component/HomePage';
 import {SearchResultsComponent} from './SearchResults.Component/SearchResultsComponent'
-=======
 import React, { Component } from "react";
-
-import "./App.css";
-import "./Include/bootstrap";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { ShoppingCart } from './ShoppingCart.Component/ShoppingCart';
-import { HomePage } from "./HomePage.Component/HomePage";
 import { AppNav } from "./Nav.Component/Nav.component.js";
->>>>>>> 13cd88b75116c8eed29205916adf4fd89fadca0e
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-<<<<<<< HEAD
-        <div id="main-content-container">
-          <Switch>
-          <Route path = "/checkout" component={CehckoutComponent} />
-          <Route path="/search" component={SearchResultsComponent}/>
-          {/* default */}
-          <Route component={HomePage} />
-          </Switch>
-        </div>
-    </BrowserRouter>
-=======
         <>
           <AppNav />
           <div id="main-content-container">
             <Switch>
+            <Route path="/search" component={SearchResultsComponent}/>
              <Route path = "/cart" component={ShoppingCart} />
               {/* default */}
               <Route component={HomePage} />
@@ -43,7 +24,6 @@ class App extends Component {
           </div>
         </>
       </BrowserRouter>
->>>>>>> 13cd88b75116c8eed29205916adf4fd89fadca0e
     );
   }
 }
