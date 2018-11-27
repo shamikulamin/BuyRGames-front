1fcf3dedf689e22ee958243ac83d8997fb493ac2
An Ta <an.thanh.ta93@gmail.coman.thanh.ta93@gmail.com>
2018-11-26 20:08:38 -0600

    shoppingCart

--- buy-r-games/src/App.js
+++ buy-r-games/src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-
 import './App.css';
 import './Include/bootstrap';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { CehckoutComponent } from './Checkout.Component/CheckoutComponent';
 import { HomePage } from './HomePage.Component/HomePage';
-
+import { ShoppingCart } from './ShoppingCart.Component/ShoppingCart';
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div id="main-content-container">
           <Switch>
-          <Route path = "/checkout" component={CehckoutComponent} />
+          <Route path = "/cart" component={ShoppingCart} />
           {/* default */}
           <Route component={HomePage} />
           </Switch>
