8ff615abe5d521b7046b722d44be6813ef097a5f
shamikulamin <shamikulamin@gmail.com>
2018-11-28 12:38:20 -0600

    merge

--- buy-r-games/src/SearchResults.Component/SearchResultsComponent.js
+++ buy-r-games/src/SearchResults.Component/SearchResultsComponent.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import '../SearchResults.Component/style.css';
 import GameClient from '../AxiosClients/GameClient';
@@ -15,7 +14,20 @@ export class SearchResultsComponent extends React.Component {
 
                 }
             ],
+            fullProducts: [
+                {
+
+                }
+            ],
             platformSort:
+                [
+
+                ],
+            genreSort:
+                [
+
+                ],
+            priceSort:
                 [
 
                 ],
@@ -24,7 +36,7 @@ export class SearchResultsComponent extends React.Component {
     }
 
 
-    sort() {
+    sortPlatform() {
         var sorted = {};
         for (var i = 0, max = this.state.Products.length; i < max; i++) {
             if (sorted[this.state.Products[i].platform] == undefined) {
@@ -36,7 +48,64 @@ export class SearchResultsComponent extends React.Component {
             ...this.state,
             platformSort: sorted
         });
-        // console.log(this.state.platformSort);
+    }
+
+    sortGenre() {
+        var sorted = {};
+        for (var i = 0, max = this.state.Products.length; i < max; i++) {
+            if (sorted[this.state.Products[i].genre] == undefined) {
+                sorted[this.state.Products[i].genre] = [];
+            }
+            sorted[this.state.Products[i].genre].push(this.state.Products[i]);
+        }
+        this.setState({
+            ...this.state,
+            genreSort: sorted
+        });
+    }
+
+    sortPrice() {
+
+        let underTen = this.state.Products.filter(function (prods) {
+            return prods.price < 10;
+        })
+
+        let tenToTwenty = this.state.Products.filter(function (prods) {
+            return (prods.price >= 10 && prods.price < 20);
+        })
+
+        let twentyToThirty = this.state.Products.filter(function (prods) {
+            return (prods.price >= 20 && prods.price < 30);
+        })
+
+        let thirtyToForty = this.state.Products.filter(function (prods) {
+            return (prods.price >= 30 && prods.price < 40);
+        })
+
+        let fortyToFifty = this.state.Products.filter(function (prods) {
+            return (prods.price >= 40 && prods.price < 50);
+        })
+
+        let fiftyToSixty = this.state.Products.filter(function (prods) {
+            return (prods.price >= 50 && prods.price < 60);
+        })
+
+        let overSixty = this.state.Products.filter(function (prods) {
+            return (prods.price >= 60);
+        })
+
+        let priceArr = {
+            "<$10": underTen,
+            "$10 - $19.99": tenToTwenty,
+            "$20 - $29.99": twentyToThirty,
+            "$30 - $39.99": thirtyToForty,
+            "$40 - $49.99": fortyToFifty,
+            "$50 - $59.99": fiftyToSixty,
+            ">$60": overSixty
+        }
+        this.setState({
+            priceSort: priceArr
+        })
     }
 
     componentWillReceiveProps(nextProps) {
@@ -46,46 +115,48 @@ export class SearchResultsComponent extends React.Component {
             this.setState({
                 ...this.state,
                 search: id
-            }, function() { GameClient.get('/games/search/' + this.state.search)
-            .then(resp => {
-                this.setState({
-                    Products: resp.data
-                })
-                // console.log(this.state.Products)
-                this.sort();
+            }, function () {
+                GameClient.get('/games/search/' + this.state.search)
+                    .then(resp => {
+                        this.setState({
+                            Products: resp.data,
+                            fullProducts: resp.data
+                        })
+                        this.sortPlatform();
+                        this.sortGenre();
+                        this.sortPrice();
+                    })
+                    .catch(err => {
+                        console.log(err);
+                    });
             })
-            .catch(err => {
-                console.log(err);
-            });})
 
         }
-        
-    }
-
-    renderData() {
-
-        console.log(this.state.search)
-        GameClient.get('/games/search/' + this.state.search)
-            .then(resp => {
-                this.setState({
-                    Products: resp.data
-                })
-                // console.log(this.state.Products)
-                this.sort();
-            })
-            .catch(err => {
-                console.log(err);
-            });
 
     }
+
 
     componentDidMount() {
         const { id } = this.props.match.params
         this.setState({
             ...this.state,
             search: id
-        },this.renderData())
-        
+        }, function () {
+            GameClient.get('/games/search/' + this.state.search)
+                .then(resp => {
+                    this.setState({
+                        Products: resp.data,
+                        fullProducts: resp.data
+                    })
+                    this.sortPlatform();
+                    this.sortGenre();
+                    this.sortPrice();
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        })
+
     }
 
 
@@ -93,16 +164,17 @@ export class SearchResultsComponent extends React.Component {
     render() {
         return (
             <>
-
                 <div class="wrapper">
 
                     <nav id="sidebar">
                         <SideBarComponent
                             pSort={this.state}
-                            modifyState={this.modifyState} />
+                            modifyState={this.modifyState}
+                            removeFilters={this.removeFilters} />
                     </nav>
 
                     <div id="content">
+                        <h2 id ="resultHeader" className="text-muted">{this.state.Products.length} Results for <strong>"{this.state.search}</strong>"</h2>
                         {this.state.Products.map(Products =>
                             <ItemComponent
                                 key={Products.product_id}
@@ -123,6 +195,13 @@ export class SearchResultsComponent extends React.Component {
             Products: narrowSearch
         })
         console.log(this.state.Products)
+    }
+
+    removeFilters = () => {
+        this.setState({
+            ...this.state,
+            Products: this.state.fullProducts
+        })
     }
 
 }
--- buy-r-games/src/SideBar.Component/SideBarComponent.js
+++ buy-r-games/src/SideBar.Component/SideBarComponent.js
@@ -3,6 +3,48 @@ import React from 'react';
 import style from '../SideBar.Component/style.css'
 
 export class SideBarComponent extends React.PureComponent {
+    constructor(props) {
+        super(props);
+        this.state = {
+            platform: "",
+            platformMap: {
+                "TG16": "TurboGrafx-16",
+                "PSP": "Sony Playstation Portable",
+                "2600": "Atari 2600",
+                "PS4": "Sony Playstation 4",
+                "N64": "Nintendo 64",
+                "GBA": "Gameboy Advanced",
+                "GG": "Game Gear",
+                "PSV": "Sony Playstation Vita",
+                "XB": "Microsoft Xbox",
+                "DC": "Sega Dreamcast",
+                "XOne": "Microsoft Xbox One",
+                "PS2": "Sony Playstation 2",
+                "Wii": "Nintendo Wii",
+                "X360": "Microsoft Xbox 360",
+                "WiiU": "Nintendo Wii U",
+                "PC": "PC",
+                "DS": "Nintendo DS",
+                "WS": "WonderSwan",
+                "2007": "Mega Touch Force 2007",
+                "PS3": "Sony Playstation 3",
+                "GEN": "Sega Genesis",
+                "SNES": "Super Nintendo",
+                "NG": "Neo Geo",
+                "GB": "Gameboy",
+                "PS": "Sony Playstation",
+                "SCD": "Sega Mega-CD",
+                "3DS": "Nintendo 3DS",
+                "3DO": "3DO Interactive Multiplayer",
+                "NES": "Nintendo Entertainment System",
+                "SAT": "Sega Saturn",
+                "GC": "Nintendo GameCude"
+            },
+
+
+        };
+
+    }
 
 
     populatePlatform = () => {
@@ -11,35 +53,70 @@ export class SideBarComponent extends React.PureComponent {
         let keys = Object.keys(data)
         let labels = [];
 
-        for(let i = 0 ; i < keys.length ; i ++){
-            labels.push(<li><button className = 'button-invis' onClick={() => this.clickFunction(data[keys[i]])} >{keys[i]} ({data[keys[i]].length})</button></li>);
+        for (let i = 0; i < keys.length; i++) {
+            labels.push(<li><button className='button-invis' onClick={() => this.clickFunction(data[keys[i]])} > {this.state.platformMap[keys[i]]} ({data[keys[i]].length})</button></li>);
+        }
+
+        return labels;
+    }
+
+    populateGenre = () => {
+
+        const data = this.props.pSort.genreSort;
+        let keys = Object.keys(data)
+        let labels = [];
+
+        for (let i = 0; i < keys.length; i++) {
+            labels.push(<li><button className='button-invis' onClick={() => this.clickFunction(data[keys[i]])} >{keys[i]} ({data[keys[i]].length})</button></li>);
+        }
+
+        return labels;
+    }
+
+    populatePrice = () => {
+
+        const data = this.props.pSort.priceSort;
+        let keys = Object.keys(data)
+        let labels = [];
+
+        for (let i = 0; i < keys.length; i++) {
+            labels.push(<li><button className='button-invis' onClick={() => this.clickFunction(data[keys[i]])} >{keys[i]} ({data[keys[i]].length})</button></li>);
         }
 
         return labels;
     }
 
     clickFunction = (data) => {
+        let x = document.getElementById("filterBtn");
+        x.style.display = "block";
         this.props.modifyState(data);
+
+    }
+
+    viewAll = () => {
+        console.log("here")
+        let x = document.getElementById("filterBtn");
+        x.style.display = "none";
+        this.props.removeFilters();
     }
 
     render() {
-        
-        
+
+
         return (
             <>
                 <div class="sidebar-header">
                     <h3>Narrow Results</h3>
+                    <button id='filterBtn' className="btn" onClick={()=> this.viewAll()}><i className="fa fa-close" ></i> Remove Filters</button>
                 </div>
 
-                <ul id = "platformUl" class="list-unstyled components">
-                    <p>Platform</p>
+                <ul id="platformUl" class="list-unstyled components">
+                    <p className = 'border-top border-bottom outset'>Platform</p>
                     {this.populatePlatform()}
-                    <li>
-                        <a href="#">Portfolio</a>
-                    </li>
-                    <li>
-                        <a href="#">Contact</a>
-                    </li>
+                    <p className = 'border-top border-bottom outset'>Genre</p>
+                    {this.populateGenre()}
+                    <p className = 'border-top border-bottom outset'>Price Range</p>
+                    {this.populatePrice()}
                 </ul>
 
 
